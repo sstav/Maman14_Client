2beac23f7c5a9e637a3fe731b65c547616e24fa8
Stav Magalnik <ststav@gmail.com>
2021-08-29 18:18:50 +0300

    Start Maman14 - Defencive Programming - Client in Python. Set Client Proccess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import socket
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -9,10 +11,34 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+class CONFIG:
+    @staticmethod
+    def port():
+        f = open("server.info", "r")
+        f = f.read().split(":")
+        return f[1]
+
+    @staticmethod
+    def ip():
+        f = open("server.info", "r")
+        f = f.read().split(":")
+        return f[0]
+
+    @staticmethod
+    def backup_list():
+        return open("server.info", "r").readlines();
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    HOST = CONFIG.ip()  # The server's hostname or IP address
+    PORT = CONFIG.port()  # The port used by the server
 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, int(PORT)))
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
+
+    print('Received', repr(data))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
