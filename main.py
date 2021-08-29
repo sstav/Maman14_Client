339d0d52b90996e21a79616f881b258b48a66a19
Stav Magalnik <ststav@gmail.com>
2021-08-29 19:37:12 +0300

    Save

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a sample Python script.
 import socket
+import os
+
 
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -26,28 +28,54 @@ class CONFIG:
 
     @staticmethod
     def backup_list():
-        return open("server.info", "r").readlines();
+        return open("server.info", "r").readlines()
+
+    @staticmethod
+    def rand_key():
+        return os.urandom(4)
+
+    @staticmethod
+    def version():
+        return 1
 
 
 class Data:
-    user_id = bytearray(4)
-    version = bytearray(1)
-    op = bytearray(1)
-    name_len = bytearray(2)
-    filename = bytearray()
-    size = bytearray(4)
-    Payload = bytearray()
+    def __init__(self):
+        self.user_id = CONFIG.rand_key()
+        self.version = bytearray(1)
+        self.op = bytearray(1)
+        self.name_len = bytearray(2)
+        self.filename = bytearray()
+        self.size = bytearray(4)
+        self.Payload = bytearray()
+        # Init data constructor:
+        self.version.append(CONFIG.version())
+
+    def request_list_backup(self):
+        self.op.append(202)
+        self.version.append(2)
+        self.userid = bytearray(4)
+        self.userid.append(2)
+        self.userid.append(2)
+        self.userid.append(2)
+        self.userid.append(2)
+
+        s.connect((HOST, int(PORT)))
+        s.sendall(self.user_id)
+        get_data = s.recv(1024)
+        print("Data Res: " + get_data)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     HOST = CONFIG.ip()  # The server's hostname or IP address
     PORT = CONFIG.port()  # The port used by the server
+    KEY = CONFIG.rand_key()
+    client = Data()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, int(PORT)))
-        s.sendall(b'Hello, world')
-        data = s.recv(1024)
+        client.request_list_backup()
 
-    print('Received', repr(data))
-
+    #print('Received', repr(data))
+    print("Bye")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
